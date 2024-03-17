6edf5cccb8c87d30da714ef8322855f27f9b0bf5
Christopher Santana <christopher@millionairepub.com>
2024-03-17 16:40:29 -0400

    Added posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,46 @@ import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../src/store';
 import Layout from '../src/layout/default';
+import posthog from 'posthog-js'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react';
+import { PostHogProvider } from 'posthog-js/react'
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug()
+    }
+  })
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog?.capture('$pageview')
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
+
+
   return (
+    <PostHogProvider client={posthog}>
     <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </Provider>
+    </PostHogProvider>
   );
 }
 
